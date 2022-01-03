efbba6bab9dd675150feecfe76c1f9572784407d
pleomorph <16924710+pleomorph@users.noreply.github.com>
2022-01-02 20:53:47 -0500

    Fix metadata

--- pushover-switch.groovy
+++ pushover-switch.groovy
@@ -3,41 +3,44 @@
  * Pushover Switch Device Handler
  */
 
-
-preferences {
- section("External Access") {
-//   input "webhook_url", "text", title: "Webhook URL", required: false
-  input "webhook_userkey", "text", title: "Pushover User Key", required: false
-  input "webhook_apptoken", "text", title: "Pushover Application Token", required: false
-  input "webhook_message", "text", title: "Custom Message", required: false
- }
-}
-
 metadata {
- definition(name: "Pushover Switch", namespace: "pleomorph", author: "Pleomorph") {
-  capability "Actuator"
-  capability "Switch"
-  capability "Sensor"
- }
+    definition(name: "Pushover Switch", namespace: "pleomorph", author: "Pleomorph") {
+        capability "Actuator"
+        capability "Switch"
+        capability "Sensor"
+    }
 
- // simulator metadata
- simulator {}
+    preferences {
+        section("External Access") {
+            //   input "webhook_url", "text", title: "Webhook URL", required: false
+            input "webhook_userkey", "text", title: "Pushover User Key", required: false
+            input "webhook_apptoken", "text", title: "Pushover Application Token", required: false
+            input "webhook_message", "text", title: "Custom Message", required: false
+        }
+    }
 
- // UI tile definitions
- tiles {
-  standardTile("button", "device.switch", width: 2, height: 2, canChangeIcon: true) {
-   state "off", label: 'Off', action: "switch.on", icon: "st.switches.switch.off", backgroundColor: "#ffffff", nextState: "on"
-   state "on", label: 'On', action: "switch.off", icon: "st.switches.switch.on", backgroundColor: "#79b821", nextState: "off"
-  }
-  standardTile("offButton", "device.button", width: 1, height: 1, canChangeIcon: true) {
-   state "default", label: 'Force Off', action: "switch.off", icon: "st.switches.switch.off", backgroundColor: "#ffffff"
-  }
-  standardTile("onButton", "device.switch", width: 1, height: 1, canChangeIcon: true) {
-   state "default", label: 'Force On', action: "switch.on", icon: "st.switches.switch.on", backgroundColor: "#79b821"
-  }
-  main "button"
-  details(["button", "onButton", "offButton"])
- }
+
+
+    // simulator metadata
+    simulator {}
+
+    // UI tile definitions
+    tiles {
+        standardTile("button", "device.switch", width: 2, height: 2, canChangeIcon: true) {
+        state "off", label: 'Off', action: "switch.on", icon: "st.switches.switch.off", backgroundColor: "#ffffff", nextState: "on"
+        state "on", label: 'On', action: "switch.off", icon: "st.switches.switch.on", backgroundColor: "#79b821", nextState: "off"
+        }
+        standardTile("offButton", "device.button", width: 1, height: 1, canChangeIcon: true) {
+        state "default", label: 'Force Off', action: "switch.off", icon: "st.switches.switch.off", backgroundColor: "#ffffff"
+        }
+        standardTile("onButton", "device.switch", width: 1, height: 1, canChangeIcon: true) {
+        state "default", label: 'Force On', action: "switch.on", icon: "st.switches.switch.on", backgroundColor: "#79b821"
+        }
+        main "button"
+        details(["button", "onButton", "offButton"])
+    }
+    
+
 }
 
 def parse(String description) {
